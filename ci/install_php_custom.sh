fba897aff458ccb9b9f73be971d5d86862760ea3
Tyson Andre <tysonandre775@hotmail.com>
2022-08-05 17:17:54 -0400

    Start testing in php 8.2

--- ci/install_php_custom.sh
+++ ci/install_php_custom.sh
@@ -31,12 +31,12 @@ if [ "x${TRAVIS:-0}" != "x" ]; then
 fi
 
 # Otherwise, put a minimal installation inside of the cache.
-if [ "$PHP_CUSTOM_NORMAL_VERSION" == "8.1.0" ] ; then
-	PHP_CUSTOM_VERSION=8.1.0beta3
+if [ "$PHP_CUSTOM_NORMAL_VERSION" == "8.2.0" ] ; then
+	PHP_CUSTOM_VERSION=8.2.0beta2
 	PHP_FOLDER="php-$PHP_CUSTOM_VERSION"
 
 	PHP_TAR_FILE="$PHP_FOLDER.tar.bz2"
-	PHP_TAR_URL=https://downloads.php.net/~ramsey/$PHP_TAR_FILE
+	PHP_TAR_URL=https://downloads.php.net/~sergey/$PHP_TAR_FILE
 else
 	PHP_FOLDER="php-$PHP_CUSTOM_VERSION"
 
